6e361418c80f743205333aae0ba1c4a437a55926
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-07-26 15:24:44 +0530

    Little modifications in schema

--- models/promotions.js
+++ models/promotions.js
@@ -3,32 +3,39 @@ require("mongoose-currency").loadType(mongoose);
 const Schema = mongoose.Schema;
 const Currency = mongoose.Types.Currency;
 
-const promoSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const promoSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
   },
-  image: {
-    type: String,
-    required: true,
-  },
-  label: {
-    type: String,
-    default: "",
-  },
-  price: {
-    type: Currency,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 var Promotions = mongoose.model("Promotion", promoSchema);
 
