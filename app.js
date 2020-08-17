039ebb86b3e3099073de17aa6fb453ebb91ac476
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-17 11:27:13 +0530

    Made a secure https server and redirected traffic from not secure to secure

--- app.js
+++ app.js
@@ -36,7 +36,17 @@ connect.then(
   },
   (err) => console.log(err)
 );
-
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
